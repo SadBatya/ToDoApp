beedc8ba31234c6c0e27cacf28dd7cc7e6dc3a3e
sadbatya <sadbatya@mail.ru>
2023-07-29 15:31:12 +0200

    fix

--- app.js
+++ app.js
@@ -3,32 +3,28 @@ const orangeBox = document.querySelector('#box2');
 const greenBox = document.querySelector('#box3');
 
 //Поменять класс "green" на "orange" и "circle" (для зеленого квадрата).
-greenBox.classList.remove('green');
-greenBox.classList.add('orange');
-greenBox.classList.add('circle');
+greenBox.addEventListener('click', () => {
+  if (
+    greenBox.classList.contains('orange') &&
+    greenBox.classList.contains('circle')
+  ) {
+    greenBox.classList.add('green');
+    greenBox.classList.remove('orange');
+    greenBox.classList.remove('circle');
+  } else {
+    greenBox.classList.remove('green');
+    greenBox.classList.add('orange');
+    greenBox.classList.add('circle');
+  }
+});
 
 //Поменять класс "orange" на "blue" (для оранжевого квадрата).
-orangeBox.classList.remove('orange');
-orangeBox.classList.add('blue');
+orangeBox.addEventListener('click', () => {
+  orangeBox.classList.remove('orange');
+  orangeBox.classList.add('blue');
+});
 
 //Добавить класс "circle" и превратить в круг (для красного квадрата).
-redBox.classList.add('circle');
-
-//
-
 redBox.addEventListener('click', () => {
-  redBox.classList.remove('circle');
+  redBox.classList.add('blue');
 });
-
-orangeBox.addEventListener('click', () => {
-  orangeBox.classList.remove('blue');
-  orangeBox.classList.add('orange');
-});
-
-greenBox.addEventListener('click', () => {
-  greenBox.classList.add('green');
-  greenBox.classList.remove('orange');
-  greenBox.classList.remove('circle');
-});
-
-
