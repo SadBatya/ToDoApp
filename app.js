f484485730eab8e2cee233708a31b184c557db8f
sadbatya <sadbatya@mail.ru>
2023-07-30 14:56:58 +0200

    fix

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const myTasksBox = document.querySelector('ul');
-
 const tasksArray = [
-  { title: 'Выучить JS', done: false, type: 'javascript' },
+  { title: 'Выучить JS', done: true, type: 'javascript' },
   { title: 'Попрактиковаться с GIT', done: false, type: 'github' },
   { title: 'Изучить React', done: false, type: 'react' },
   { title: 'Понять NodeJS', done: false, type: 'nodejs' },
   { title: 'Устроиться на работу', done: false, type: 'job' },
 ];
 
+
+
 const tasks = tasksArray.map(task => {
   return `
      <li>
         <label data-type=${task.type}>
-          <input type="checkbox" checked /> ${task.title}
+          <input type="checkbox" ${task.done ? 'checked' : ''} /> ${task.title}
         </label>
       </li>
   `
