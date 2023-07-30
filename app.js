fdd463a692fbe6598cc1a374775f1fedf4eebef9
sadbatya <sadbatya@mail.ru>
2023-07-30 21:36:10 +0200

    tasks

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const modal = document.querySelector('#modal');
 const content = document.querySelector('#content');
 const backdrop = document.querySelector('#backdrop');
 const progress = document.querySelector('#progress');
-
+const APP_TITLE = document.title
 const technologies = [
   { title: 'HTML', description: 'HTML text', type: 'html', done: true },
   { title: 'CSS', description: 'CSS text', type: 'css', done: true },
@@ -16,11 +16,42 @@ const technologies = [
   { title: 'Git', description: 'GIT text', type: 'git', done: false },
 ];
 
-content.addEventListener('click', (openCard) => {
-  modal.classList.add('open');
+modal.addEventListener('change', toogleTech = (e) => {
+  const type = e.target.dataset.type
+  const tech  = technologies.find(t => t.type === type)
+  tech.done = e.target.checked
+  init()
+})
+
+
+content.addEventListener('click', (e) => {
+  const data = e.target.dataset
+  const tech  = technologies.find(t => t.type === data.type)
+  if(!tech) return
+  openModal(toModal(tech), tech.title)
 });
 
+function toModal(tech){
+  const checked = tech.done ? 'checked' : ''
+  return `
+  <h2>${tech.title}</h2>
+      <p>${tech.description}</p>
+      <hr />
+      <div>
+        <input type="checkbox" id="done" ${checked} data-type='${tech.type}'/>
+        <label for="done">Выучил</label>
+      </div>
+  `
+}
+
+function openModal(html, title = APP_TITLE){
+  document.title = `${title} | ${APP_TITLE}`
+  modal.innerHTML = html
+  modal.classList.add('open')
+}
+
 backdrop.addEventListener('click', (closeModal) => {
+  document.title = APP_TITLE
   modal.classList.remove('open');
 });
 
@@ -68,14 +99,13 @@ function computeProgressPercent() {
       doneCount++;
     }
   }
-  console.log(doneCount);
   return Math.floor((100 * doneCount) / technologies.length);
 }
 
 function toCard(tech) {
   const doneClass = tech.done ? 'done' : '';
-  return `<div class='card ${doneClass}'>
-          <h3>${tech.title}</h3>
+  return `<div class='card ${doneClass}' data-type='${tech.type}'>
+          <h3 data-type='${tech.type}'>${tech.title}</h3>
         </div> `;
 }
 
