7348be612b70e5194ba945bbe7f78fce8a1a5107
sadbatya <sadbatya@mail.ru>
2023-07-30 14:26:03 +0200

    fix

--- app.js
+++ app.js
@@ -4,20 +4,18 @@ const normalizeBtn = document.querySelector('#normal');
 const fontWeightBoldBtn = document.querySelector('#bold');
 const fontStyleItalicdBtn = document.querySelector('#italic');
 const text = document.querySelector('h3');
-const styleText = window.getComputedStyle(text)
-let fontSizeText = parseInt(styleText.fontSize)
-
-
+const styleText = window.getComputedStyle(text);
+let fontSizeText = parseInt(styleText.fontSize);
 
 plusFontSizeBtn.addEventListener('click', () => {
-  fontSizeText++
-  text.style.fontSize = fontSizeText + 'px'
+  fontSizeText++;
+  text.style.fontSize = fontSizeText + 'px';
 });
 
 minusFontSizeBtn.addEventListener('click', () => {
-  fontSizeText--
-  text.style.fontSize = fontSizeText + 'px'
-})
+  fontSizeText--;
+  text.style.fontSize = fontSizeText + 'px';
+});
 
 fontWeightBoldBtn.addEventListener('click', () => {
   text.style.fontWeight = 'bold';
@@ -31,4 +29,3 @@ normalizeBtn.addEventListener('click', () => {
   text.style.fontWeight = 'normal';
   text.style.fontStyle = 'normal';
 });
- 
