610a921b3e51f06c526137348b783de9a6f557f3
sadbatya <sadbatya@mail.ru>
2023-07-30 14:17:45 +0200

    task5

--- app.js
+++ app.js
@@ -1,43 +1,32 @@
-const modal = document.querySelector('#modal');
-const content = document.querySelector('#content');
-const backdrop = document.querySelector('#backdrop');
+const plusFontSizeBtn = document.querySelector('#plus');
+const minusFontSizeBtn = document.querySelector('#minus');
+const normalizeBtn = document.querySelector('#normal');
+const fontWeightBoldBtn = document.querySelector('#bold');
+const fontStyleItalicdBtn = document.querySelector('#italic');
+const text = document.querySelector('h3');
+let fontSizeValue = 20
 
-const technologies = [
-  {title: 'HTML', description: 'HTML text', type: 'html', done: true},
-  {title: 'CSS', description: 'CSS text', type: 'css', done: true},
-  {title: 'JavaScript', description: 'JS text', type: 'javascript', done: false},
-  {title: 'React', description: 'React text', type: 'react', done: false},
-  {title: 'Git', description: 'GIT text', type: 'git', done: false},
-];
 
-content.addEventListener('click', (openCard) => {
-  modal.classList.add('open');
+plusFontSizeBtn.addEventListener('click', () => {
+  fontSizeValue++
+  text.style.fontSize = fontSizeValue + 'px'
 });
 
-backdrop.addEventListener('click', (closeModal) => {
-  modal.classList.remove('open');
+minusFontSizeBtn.addEventListener('click', () => {
+  fontSizeValue--
+  text.style.fontSize = fontSizeValue + 'px'
+})
+
+fontWeightBoldBtn.addEventListener('click', () => {
+  text.style.fontWeight = 'bold';
 });
 
-function init() {
-  if (technologies.length === 0) {
-    content.innerHTML = `<p class='empty'>Нет задач</p>`;
-  } else {
-    let html = '';
-    for (let i = 0; i < technologies.length; i++) {
-      const tech = technologies[i];
-      html += toCard(tech)
-    }
-    content.innerHTML = html
-    // content.innerHTML = technologies.map(toCard).join('');
-  }
-}
+fontStyleItalicdBtn.addEventListener('click', () => {
+  text.style.fontStyle = 'italic';
+});
 
-function toCard(tech) {
-  const doneClass = tech.done ? 'done' : ''
-  return`<div class='card ${doneClass}'>
-          <h3>${tech.title}</h3>
-        </div> `;
-}
-
-
-init()
+normalizeBtn.addEventListener('click', () => {
+  text.style.fontWeight = 'normal';
+  text.style.fontStyle = 'normal';
+});
+ 
