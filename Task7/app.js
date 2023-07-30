f2eee150b6df070a7427a9517d645a5717feb762
sadbatya <sadbatya@mail.ru>
2023-07-30 23:13:33 +0200

    task7

---
+++ Task7/app.js
@@ -0,0 +1,14 @@
+const form = document.querySelector('form')
+const tasks = document.querySelector('.list')
+const btn = document.querySelector('button')
+const input = document.querySelector('input')
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+})
+
+btn.addEventListener('click', () => {
+  const task = document.createElement('li')
+  task.innerText = input.value
+  tasks.append(task)
+  input.value = ''
+})
