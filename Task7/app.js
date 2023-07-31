6ff3707735c020b55a65a6a97fc14d9ae2e1eda8
sadbatya <sadbatya@mail.ru>
2023-07-31 10:03:12 +0200

    fix

--- Task7/app.js
+++ Task7/app.js
@@ -1,15 +1,21 @@
-const form = document.querySelector('form')
-const tasks = document.querySelector('.list')
-const btn = document.querySelector('button')
-const input = document.querySelector('input')
+const form = document.querySelector('form');
+const tasks = document.querySelector('.list');
+const btn = document.querySelector('button');
+const input = document.querySelector('input');
 
 form.addEventListener('submit', (e) => {
-  e.preventDefault()
-})
+  e.preventDefault();
+});
 
 btn.addEventListener('click', () => {
-  const task = document.createElement('li')
-  task.innerText = input.value
-  tasks.append(task)
-  input.value = ''
-})
+  if (input.value.trim() === '') {
+    input.style.border = 'red solid 1px'
+    setTimeout(() => {input.style.border = '1px solid rgba(151, 158, 162, 0.3)'}, 1000)
+    input.value = ''
+  } else {
+    const task = document.createElement('li');
+    task.innerText = input.value;
+    tasks.append(task);
+    input.value = '';
+  }
+});
